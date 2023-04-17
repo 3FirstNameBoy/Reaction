70d219d284f0f475eaa77f7eb20781a9afaed9b7
Benjamin Brandon <benb89.bb@gmail.com>
2023-04-16 23:46:35 -0400

    creation of a state folder for reducers andactions for the rest of the application

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useReducer } from "react";
+import reducer, { initialState } from "./state/reducer";
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  console.log("state", state);
+
   return (
     <div>
-     <h2>Reaction</h2>
+      <h2>Reaction</h2>
     </div>
   );
 }
